afd856bfdcbe2c48aba1afc2fede02d0982075cb
feministAF <santiagoreha24@gmail.com>
2019-09-20 23:53:23 +0530

    almost done with front end

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 ///version of solidity
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task{
+        uint task;
+        string content;
+        bool completed;
+    }
+    mapping(uint =>  Task) public tasks;
+    ///like a key value pair
+
+    constructor() public{
+        createTask("hello");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -2,8 +2,8 @@ module.exports = {
   networks:{
     development:{
       host:"127.0.0.1",//localhost
-      post:7545,//port where ganache is running 
-      netword_id:"*"//match any network id
+      port:7545,//port where ganache is running 
+      network_id:"*"//match any network id
     }
   },
   solc:{
