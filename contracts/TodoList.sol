bb2bac0ae81c782ec80493171f7f3fc387e6cf59
feministAF <santiagoreha24@gmail.com>
2019-09-21 12:57:42 +0530

    creates tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,12 +4,17 @@ contract TodoList{
     uint public taskCount = 0;
 
     struct Task{
-        uint task;
+        uint id;
         string content;
         bool completed;
     }
     mapping(uint =>  Task) public tasks;
     ///like a key value pair
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
 
     constructor() public{
         createTask("hello");
@@ -18,5 +23,6 @@ contract TodoList{
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount,_content,false);
     }
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -20,4 +20,13 @@ contract('TodoList',(accounts) => {
         assert.equal(task.completed, false)
         assert.equal(taskCount.toNumber(), 1)
       })
+      it('creates tasks',async()=>{
+          const result = await this.todoList.createTask('say goodbye')
+          const taskCount = await this.todoList.taskCount()
+          assert.equal(taskCount,2)
+          const event = result.logs[0].args
+          assert.equal(event.id.toNumber(),2)
+          assert.equal(event.content,'say goodbye')
+          assert.equal(event.completed,false)
+      })
 })
