2f5f1c7adcade390fdb9cbf3ea093e0308614c07
feministAF <santiagoreha24@gmail.com>
2019-09-21 12:35:25 +0530

    tests done

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks:{
     development:{
       host:"127.0.0.1",//localhost
-      port:7545,//port where ganache is running 
+      port:8545,//port where ganache is running 
       network_id:"*"//match any network id
     }
   },
