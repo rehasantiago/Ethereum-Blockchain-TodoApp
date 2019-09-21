eaeb4fb4787bd0cbe581abacfbc7eb07ad9fa7e0
feministAF <santiagoreha24@gmail.com>
2019-09-21 13:37:33 +0530

    toggle completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -16,6 +16,11 @@ contract TodoList{
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public{
         createTask("hello");
     }
@@ -24,5 +29,12 @@ contract TodoList{
         taskCount++;
         tasks[taskCount] = Task(taskCount,_content,false);
         emit TaskCreated(taskCount,_content,false);
+    }
+
+    function toggleCompleted(uint _id) public{
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id,_task.completed);
     }
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -29,4 +29,12 @@ contract('TodoList',(accounts) => {
           assert.equal(event.content,'say goodbye')
           assert.equal(event.completed,false)
       })
+      it('toggles task completion',async()=>{
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal(task.completed,true)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(),1)
+        assert.equal(event.completed,true)
+    })
 })
